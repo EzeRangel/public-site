1ff8d2af6a3333e375b470dc7f2e05a72fcf7c22
EzeRangel <chekelinho@gmail.com>
2018-11-30 17:10:03 -0700

    Add easter egg :)

--- public/theme/base-2017_dev/source/js/app.js
+++ public/theme/base-2017_dev/source/js/app.js
@@ -59,5 +59,70 @@ APP.Interactions.prototype = {
     }
 };
 
+//
+
+APP.KC = function(){
+    this.init();
+}
+
+APP.KC.prototype = {
+    init: () => {
+        const allowedKeys = {
+          37: 'left',
+          38: 'up',
+          39: 'right',
+          40: 'down',
+          65: 'a',
+          66: 'b'
+        };
+
+        const konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+
+        function activateCheats() {
+            var style = document.createElement('link');
+
+            style.onload = function(){
+                document.body.classList.add("t-nintendo")
+
+                alert("WHAT A HORRIBLE NIGHT TO HAVE A CURSE.");
+            }
+
+            style.setAttribute("href", "https://unpkg.com/nes.css@0.0.2/css/nes.min.css");
+            style.setAttribute("rel", "stylesheet")
+
+            document.head.appendChild(style);
+
+        }
+
+        // a variable to remember the 'position' the user has reached so far.
+        var konamiCodePosition = 0;
+
+        // add keydown event listener
+        document.addEventListener('keydown', function(e) {
+          // get the value of the key code from the key map
+          var key = allowedKeys[e.keyCode];
+          // get the value of the required key from the konami code
+          var requiredKey = konamiCode[konamiCodePosition];
+
+          // compare the key with the required key
+          if (key == requiredKey) {
+
+            // move to the next key in the konami code sequence
+            konamiCodePosition++;
+
+            // if the last key is reached, activate cheats
+            if (konamiCodePosition == konamiCode.length) {
+              activateCheats();
+              konamiCodePosition = 0;
+              console.log("Done")
+            }
+          } else {
+            konamiCodePosition = 0;
+          }
+        });
+    }
+}
+
 new APP.Document();
 new APP.Interactions();
+new APP.KC();
